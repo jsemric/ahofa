92e9462d8b7c1769b4484fd6a49e30e42e5fa30a
jsemric <jakubsemric@gmail.com>
2017-08-31 16:13:41 +0200

    Refactoring reduction.

--- reduce_nfa.py
+++ reduce_nfa.py
@@ -5,13 +5,6 @@ from collections import defaultdict
 import os, sys
 import argparse
 
-class AutomatonException(Exception):
-    def __init__(self, arg):
-        self.arg = arg
-
-    def __str__(self):
-        return self.arg
-
 class NetworkNFA:
 
     RE_transition_FA_format = re.compile('^\w+\s+\w+\s+\w+$')
@@ -19,62 +12,78 @@ class NetworkNFA:
     RE_state_freq_FA_format = re.compile('^\w+\s+\d+(\s+\d+)?$')
 
     def __init__(self):
-        self.transitions = list(defaultdict(set))
-        self.initial_states = set()
-        self.final_states = set()
-        self.state_map = dict()
-        self.state_count = 0
-        self.state_freq = list()
-        self.succ = list()
-        self.pred = list()
-        self.state_depth = list()
-        self.total_freq = 0
+        self._transitions = list(defaultdict(set))
+        self._initial_states = set()
+        self._final_states = set()
+        self._state_map = dict()
+        self._state_freq = list()
+        self._state_depth = list()
+
+    @property
+    def total_freq(self):
+        return max(self._state_freq)
+
+    @property
+    def state_count(self):
+        return len(self._transitions)
+
+    @property
+    def pred(self):
+        pred = [set() for i in range(self.state_count)]
+
+        for state, rules in enumerate(self._transitions):
+            for key, value in rules.items():
+                for q in value:
+                    pred[q].add(state)
+
+        return pred
+
+    @property
+    def succ(self):
+        succ = [set() for i in range(self.state_count)]
+
+        for state, rules in enumerate(self._transitions):
+            for key, value in rules.items():
+                for q in value:
+                    succ[state].add(q)
+
+        return succ
 
     def _add_state(self, state):
-        if not state in self.state_map:
-            self.transitions.append(defaultdict(set))
-            self.state_map[state] = self.state_count
-            self.state_count += 1
+        if not state in self._state_map:
+            self._transitions.append(defaultdict(set))
+            self._state_map[state] = len(self._state_map)
 
-        return self.state_map[state]
+        return self._state_map[state]
 
     def _add_rule(self, pstate, qstate, symbol):
         pstate = self._add_state(pstate)
         qstate = self._add_state(qstate)
-        self.transitions[pstate][int(symbol,0)].add(qstate)
+        self._transitions[pstate][int(symbol,0)].add(qstate)
 
     def _add_initial_state(self, state):
         state = self._add_state(state)
-        self.initial_states.add(state)
+        self._initial_states.add(state)
 
     def _add_final_state(self, fstate):
-        self.final_states.add(self.state_map[fstate])
+        self._final_states.add(self._state_map[fstate])
 
     def _add_freq(self, state, freq, depth=None):
-        state = self.state_map[state]
-        self.state_freq[state] = int(freq)
-
-    def _compute_pred_and_succ(self):
-        self.succ = [set() for i in range(self.state_count)]
-        self.pred = [set() for i in range(self.state_count)]
-
-        for state, rules in enumerate(self.transitions):
-            for key, value in rules.items():
-                for q in value:
-                    self.succ[state].add(q)
-                    self.pred[q].add(state)
+        state = self._state_map[state]
+        self._state_freq[state] = int(freq)
 
     def _compute_depth(self):
-        self.state_depth = [0 for i in range(self.state_count)]
-        actual = self.initial_states.copy()
+        succ = self.succ
+        self._state_depth = [0 for i in range(self.state_count)]
+        actual = self._initial_states.copy()
         empty = set()
         depth = 0
         while True:
             empty = empty.union(actual)
             new = set()
             for q in actual:
-                self.state_depth[q] = depth
-                new = new.union(self.succ[q])
+                self._state_depth[q] = depth
+                new = new.union(succ[q])
             new -= empty
             actual = new
             if not new:
@@ -87,18 +96,19 @@ class NetworkNFA:
     ###########################################################################
 
     def write_fa(self):
-        yield from self.initial_states
-        for state, rules in enumerate(self.transitions):
+        yield from self._initial_states
+        for state, rules in enumerate(self._transitions):
             for key, value in rules.items():
                 for q in value:
                     yield ' '.join((str(state),str(q),hex(key)))
-        yield from self.final_states
+        yield from self._final_states
 
     def print_fa(self):
         for line in self.write_fa():
             print(line)
 
-    def parse_fa_file(self, fname):
+    def parse_fa_file(fname):
+        res = NetworkNFA()
         rules = 0
         with open(fname, 'r') as f:
             for line in f:
@@ -109,41 +119,41 @@ class NetworkNFA:
                 if rules == 0:
                     # read initial state(s)
                     if NetworkNFA.RE_state_FA_format.match(line):
-                        self._add_initial_state(line)
+                        res._add_initial_state(line)
                     elif NetworkNFA.RE_transition_FA_format.match(line):
                         rules = 1
-                        self._add_rule(*line.split())
+                        res._add_rule(*line.split())
                     else:
-                        raise AutomatonException('invalid syntax ' + line)
+                        raise RuntimeError('invalid syntax: \"' + line + '\"')
                 elif rules == 1:
                     # read transitions
                     if NetworkNFA.RE_transition_FA_format.match(line):
-                        self._add_rule(*line.split())
+                        res._add_rule(*line.split())
                     elif NetworkNFA.RE_state_FA_format.match(line):
-                        self._add_final_state(line)
+                        res._add_final_state(line)
                         rules = 2
                     else:
-                        raise AutomatonException('invalid syntax: ' + line)
+                        raise RuntimeError('invalid syntax: \"' + line + '\"')
                 elif rules == 2:
                     # read final states
                     if NetworkNFA.RE_state_FA_format.match(line):
-                        self._add_final_state(line)
+                        res._add_final_state(line)
                     elif line.startswith('====='):
-                        self.state_freq = [0 for x in range(self.state_count)]
+                        res._state_freq = [0 for x in range(res.state_count)]
                         rules = 3
                     else:
-                        raise AutomatonException('invalid syntax: ' + line)
+                        raise RuntimeError('invalid syntax: \"' + line + '\"')
                 else:
                     if NetworkNFA.RE_state_freq_FA_format.match(line):
-                        self._add_freq(*line.split())
+                        res._add_freq(*line.split())
                     else:
-                        raise AutomatonException('invalid syntax: ' + line)
+                        raise RuntimeError('invalid syntax: \"' + line + '\"')
 
-        self._compute_pred_and_succ()
-        self._compute_depth()
-        self.total_freq = max(self.state_freq)
-        for x in self.initial_states:
-            self.state_freq[x] = self.total_freq
+        res._compute_depth()
+        for x in res._initial_states:
+            res._state_freq[x] = res.total_freq
+
+        return res
 
 
     ###########################################################################
@@ -151,18 +161,19 @@ class NetworkNFA:
     ###########################################################################
 
     def add_selfloops_to_final_states(self):
-        for x in self.final_states:
+        for x in self._final_states:
             for c in range(256):
-                self.transitions[x][c] = set([x])
+                self._transitions[x][c] = set([x])
 
     def remove_unreachable(self):
-        actual = self.initial_states
+        succ = self.succ
+        actual = self._initial_states
         reached = set()
         while True:
             reached = reached.union(actual)
             new = set()
             for q in actual:
-                new = new.union(self.succ[q])
+                new = new.union(succ[q])
             new -= reached
             actual = new
             if not new:
@@ -175,32 +186,30 @@ class NetworkNFA:
             cnt += 1
 
         out = NetworkNFA()
-        out.state_count = cnt
-        out.transitions = [defaultdict(set) for x in range(cnt)]
-        for state, rules in enumerate(self.transitions):
+        out._transitions = [defaultdict(set) for x in range(cnt)]
+        for state, rules in enumerate(self._transitions):
             if state in state_map:
                 for key, val in rules.items():
-                    out.transitions[state_map[state]][key] = set([state_map[x] \
+                    out._transitions[state_map[state]][key] = set([state_map[x] \
                     for x in val if x in state_map])
 
-        out.initial_states = [state_map[x] for x in self.initial_states]
-        out.final_states = [state_map[x] for x in self.final_states \
+        out._initial_states = [state_map[x] for x in self._initial_states]
+        out._final_states = [state_map[x] for x in self._final_states \
                            if x in state_map]
-        out._compute_pred_and_succ()
         out._compute_depth()
 
         return out
 
 
     def state_error(self, state):
-        sf = self.state_freq[state]
+        sf = self._state_freq[state]
         return 0 if sf == 0 else sf / self.total_freq
 
     def reduce(self, error=0, depth=0):
         marked = set()
         srt_states = sorted([x for x in range(self.state_count) \
-                            if self.state_depth[x] >= depth], \
-                            key=lambda x : self.state_freq[x])
+                            if self._state_depth[x] >= depth], \
+                            key=lambda x : self._state_freq[x])
 
         for state in srt_states:
             err = self.state_error(state)
@@ -213,20 +222,18 @@ class NetworkNFA:
 
         final_state_label = self.state_count
         new_transitions = [defaultdict(set) for x in range(self.state_count+1)]
-        for state, rules in enumerate(self.transitions):
+        for state, rules in enumerate(self._transitions):
             for key, val in rules.items():
                 new_transitions[state][key] = \
                 set([x if x not in marked else final_state_label for x in val])
 
         out = NetworkNFA()
-        out.transitions = new_transitions
-        out.state_count = self.state_count + 1
-        out.final_states = self.final_states.copy()
-        out.initial_states = self.initial_states.copy()
-        out.final_states.add(final_state_label)
-        out._compute_pred_and_succ()
+        out._transitions = new_transitions
+        out._final_states = self._final_states.copy()
+        out._initial_states = self._initial_states.copy()
+        out._final_states.add(final_state_label)
         out = out.remove_unreachable()
-        sys.stderr.write(str(out.state_count) + '/' + str(self.state_count) + '\n')
+        sys.stderr.write('{}/{}\n'.format(out.state_count,self.state_count))
 
         return out
 
@@ -257,8 +264,7 @@ def main():
         sys.stderr.write('Error: no input specified\n')
         sys.exit(1)
 
-    a = NetworkNFA()
-    a.parse_fa_file(args.input)
+    a = NetworkNFA.parse_fa_file(args.input)
 
     a = a.reduce(args.max_error, args.depth)
     if args.add_sl:
