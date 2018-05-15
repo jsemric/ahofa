7df88e3383eca0bdb1c1a4553b7b9b7fbba5d13b
jsemric <jakubsemric@gmail.com>
2018-05-15 17:18:55 +0200

    program options

--- app-reduction.py
+++ app-reduction.py
@@ -10,26 +10,28 @@ from nfa import Nfa
 from reduction_eval import reduce_nfa, armc
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Approximate NFA reduction.')
     parser.add_argument('-r','--ratio', metavar='N', type=float,
         default=.2, help='reduction ratio')
     parser.add_argument('input', type=str, help='NFA to reduce')
     parser.add_argument('-n','--nw', type=int,
         default=multiprocessing.cpu_count() - 1,
         help='number of workers to run in parallel')
-    parser.add_argument('--test', type=str, help='test pcap files')
-    parser.add_argument('--train', type=str, help='train pcap file')
+    parser.add_argument('--test', nargs='+', type=str,  metavar='PCAP',
+        help='test pcap files')
+    parser.add_argument('--train', type=str, metavar='PCAP',
+        help='train pcap file')
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-m','--merge', action='store_true',
         help='merging reduction')
     group.add_argument('-a','--armc', action='store_true',
-        help='merging reduction ala abstract regular model checking')
+        help='merging reduction inspired by abstract regular model checking')
 
-    parser.add_argument('-th','--thresh', type=float,
+    parser.add_argument('-th','--thresh', type=float, metavar='N',
         help='threshold for merging', default=.995)
     parser.add_argument('-mf','--maxfr', type=float, default=.1,
-        help='max frequency of a state allowed to be merged')
+         metavar='N', help='max frequency of a state allowed to be merged')
     parser.add_argument('-o','--output', type=str,default='output.fa')
     args = parser.parse_args()
 
@@ -43,24 +45,32 @@ def main():
         # merge using armc and prune
         aut, m = armc(aut, args.train, ratio=args.ratio, th=args.thresh,
             merge_empty=False)
-        sys.stderr.write('Merged: ' + str(m) + '\n')
+        sys.stderr.write('states merged: ' + str(m) + '\n')
     else:
-        sys.stderr.write('Reduction ratio: ' + str(args.ratio) + '\n')
+        sys.stderr.write('reduction ratio: ' + str(args.ratio) + '\n')
         freq = aut.get_freq(args.train)
         aut, m = reduce_nfa(aut, freq, ratio=args.ratio, merge=args.merge,
             th=args.thresh, mf=args.maxfr)
         if args.merge:
-            sys.stderr.write('Merged: ' + str(m) + '\n')
+            sys.stderr.write('states merged: ' + str(m) + '\n')
 
     with open(args.output,'w') as f:
-        sys.stderr.write('Saved as ' + args.output + '\n')
+        sys.stderr.write('saved as ' + args.output + '\n')
         aut.print(f)
 
     if args.test:
+        sys.stderr.write('evaluation reduction error\n')
         reduced = args.output
-        r = Nfa.eval_accuracy(args.input, args.output, args.test, nw=args.nw)
-        _,_,total, _, _, fp, tp = r.split(',')
-        total, fp, tp = int(total), int(fp), int(tp)
+        r = Nfa.eval_accuracy(args.input, args.output, ' '.join(args.test),
+            nw=args.nw)
+        total, fp, tp = 0, 0, 0
+        for b in r.split('\n'):
+            if b != '':
+                _, _, s1, _, _, s2, s3 = b.split(',')
+                total += int(s1)
+                fp += int(s2)
+                tp += int(s3)
+
         print('error:', round(fp/total,4))
         if tp + fp > 0:
             print('precision:', round(tp/(fp+tp),4))
